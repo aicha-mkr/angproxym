22b5dbba281789b8d7ef6e69502e4690c1f40f8a
intissar.ben-zouari <intissar.ben-zouari@valomnia.com>
2024-08-19 11:28:59 +0100

    add login module api

--- laravel-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,27 +14,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('organisation')->nullable();
+            $table->string('role')->nullable();
+            $table->string('token')->nullable();
+            $table->string('cookies')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
     }
 
     /**
